815d8f61cdce251b95c089b42bcb9807e2ddc758
Paulo Santos <peedesantos40@gmail.com>
2026-07-01 10:48:19 -0300

    Responcivida no projeto

--- src/componentes/Rodape/index.jsx
+++ src/componentes/Rodape/index.jsx
@@ -5,12 +5,12 @@ export default function Rodape() {
     return (
         <footer className={estilos.rodape}>
             <div className={estilos.container_rodape}>
-                <div>
+                <div className={estilos.logo_container}>
                     <Image src="/logo.png" alt="Logo da Sorveteria" width={200} height={200} />
                 </div>
                 <div className={estilos.container_texto}>
                     <h3>ENDEREÇO</h3>
-                    <p>Av. Bernardino de Campos, </p>  
+                    <p>Av. Bernardino de Campos, </p>
                     <p>98 São Paulo, SP 12355-678.</p>
                 </div>
                 <div className={estilos.container_texto}>
@@ -22,8 +22,6 @@ export default function Rodape() {
                     <h3>HORÁRIO</h3>
                     <p>ABERTO TODOS OS DIAS</p>
                     <p>10:00 - 22:00</p>
-                </div>
-                <div>
                 </div>
             </div>
             <p className={estilos.container_texto_rodape}>© 2024 Sorveteria Artesanal. Todos os direitos reservados.</p>
