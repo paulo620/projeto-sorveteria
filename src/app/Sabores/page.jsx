9cb07f453cf546d13a5c0d5c55690c2d3aa9d764
Paulo Santos <peedesantos40@gmail.com>
2026-07-01 15:09:28 -0300

    Novos ajustes

--- src/app/Sabores/page.jsx
+++ src/app/Sabores/page.jsx
@@ -11,34 +11,34 @@ export default function Sabores() {
                 <h2>SABORES DE SORVETE</h2>
                     <div className={estilos.cards_sabores}>
                         <div className={estilos.sabores_sorvete}>
-                            <Image src="/sabor-oreo.png" alt="Sorvete 1" width={300} height={300} />
+                            <Image src="/sabor-oreo.png" alt="Sorvete de Oreo" width={300} height={300} />
                             <span>Sorvete de Oreo</span>
                             <p>Delicioso sorvete sabor Oreo. Uma explosão de sabor.</p>
                         </div>
                         <div className={estilos.sabores_sorvete}>
-                            <Image src="/sabor-pistache.png" alt="Sorvete 2" width={300} height={300} />
+                            <Image src="/sabor-pistache.png" alt="Sorvete de Pistache" width={300} height={300} />
                             <span>Sorvete de Pistache</span>
                             <p>Cremoso sorvete sabor pistache com pedacinhos de semente.</p>
                         </div>
                         <div className={estilos.sabores_sorvete}>
-                            <Image src="/sabor-cookies-avela.png" alt="Sorvete 3" width={300} height={300} />
+                            <Image src="/sabor-cookies-avela.png" alt="Sorvete Cookies & Avelã" width={300} height={300} />
                             <span>Sorvete Cookies & Avelã</span>
                             <p>O nosso melhor sorvete. Você vai adorar o sabor.</p>
                         </div>
                     </div>
                     <div className={estilos.cards_sabores}>
                         <div className={estilos.sabores_sorvete}>
-                            <Image src="/sorbet-kiwi.png" alt="Sorvete 4" width={300} height={300} />
+                            <Image src="/sorbet-kiwi.png" alt="Sorvete de Kiwi" width={300} height={300} />
                             <span>Sorvete de Kiwi</span>
                             <p>Delicioso e refrescante sorvete sabor kiwi. Rico em vitamina C.</p>
                         </div>
                         <div className={estilos.sabores_sorvete}>
-                            <Image src="/sorbet-morango.png" alt="Sorvete 1" width={300} height={300} />
+                            <Image src="/sorbet-morango.png" alt="Sorvete de Morango" width={300} height={300} />
                             <span>Sorvete de Morango</span>
                             <p>Sorvete de morango gourmet. Tradicional e saboroso.</p>
                         </div>
                         <div className={estilos.sabores_sorvete}>
-                            <Image src="/sorbet-limao.png" alt="Sorvete 5" width={300} height={300} />
+                            <Image src="/sorbet-limao.png" alt="Sorvete de Limão" width={300} height={300} />
                             <span>Sorvete de Limão</span>
                             <p>O incrivel sorvete gourmet de limão siciliano vai te encantar.</p>
                         </div>
